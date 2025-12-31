6be152dad34fccf3a5e89c2b745a939a764fcb8c
Farhan Ramadhan <farhan15r@area51.local>
2025-12-31 09:26:53 +0700

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'dist',
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
