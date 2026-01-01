a250c6c7d81ab2047d1e1e2a4e1d98f34b0bc8bb
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2026-01-01 16:55:14 +0700

    update: add github action

--- src/app/robots.js
+++ src/app/robots.js
@@ -2,11 +2,26 @@ export const dynamic = "force-static";
 
 export default function robots() {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
-    sitemap: 'https://halo-toyota.com/sitemap.xml',
+    rules: [
+      {
+        userAgent: [
+          'Amazonbot',
+          'Applebot-Extended',
+          'Bytespider',
+          'CCBot',
+          'ClaudeBot',
+          'Google-Extended',
+          'GPTBot',
+          'meta-externalagent',
+        ],
+        disallow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
+    sitemap: 'https://halotoyota.com/sitemap.xml',
   }
 }
