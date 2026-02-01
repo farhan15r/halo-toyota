f5fa8b00d0c59f45b5b4bc58165e9b9c5091f6c0
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2026-02-01 14:20:39 +0700

    feat: Add car image assets and their structured data, and update CarGallery to dynamically render car images.

--- src/components/CarGallery.js
+++ src/components/CarGallery.js
@@ -1,144 +1,33 @@
+import listCarImages from '../data/listCarImages.json';
+
 export default function CarGallery() {
     return (
         <section className="py-10 justify-center flex flex-col items-center">
             <h2 className="text-2xl font-bold mb-4">Mobil</h2>
 
             <div className="flex flex-wrap gap-6 max-w-300 justify-center">
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-70 rounded-2xl">
-                        <img src="/mvp_veloz-black-metallic.png" alt="3D card" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-70 rounded-2xl">
-                        <img src="/mvp_veloz-black-metallic.png" alt="3D card" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-70 rounded-2xl">
-                        <img src="/mvp_veloz-black-metallic.png" alt="3D card" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-70 rounded-2xl">
-                        <img src="/mvp_veloz-black-metallic.png" alt="3D card" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-70 rounded-2xl">
-                        <img src="/mvp_veloz-black-metallic.png" alt="3D card" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-70 rounded-2xl">
-                        <img src="/mvp_veloz-black-metallic.png" alt="3D card" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-70 rounded-2xl">
-                        <img src="/mvp_veloz-black-metallic.png" alt="3D card" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-70 rounded-2xl">
-                        <img src="/mvp_veloz-black-metallic.png" alt="3D card" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="hover-3d">
-                    {/* content */}
-                    <figure className="w-70 rounded-2xl">
-                        <img src="/mvp_veloz-black-metallic.png" alt="3D card" />
-                    </figure>
-                    {/* 8 empty divs needed for the 3D effect */}
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
+                {listCarImages.map((car, index) => {
+                    const firstImageKey = Object.keys(car.images)[0];
+                    const imageUrl = car.images[firstImageKey];
+
+                    return (
+                        <div key={index} className="hover-3d">
+                            {/* content */}
+                            <figure className="w-70 rounded-2xl">
+                                <img src={imageUrl} alt={car.name} />
+                            </figure>
+                            {/* 8 empty divs needed for the 3D effect */}
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    );
+                })}
             </div>
         </section>
     )
